ef083c73055a0a0273dd95dbeeb42882f48574a3
imsourceboom <ecmascript.developer@gmail.com>
2020-02-07 12:14:53 +0900

    delete gulp folder github

--- gulp/errorHandler.js
+++
@@ -1,8 +0,0 @@
-import gutil from 'gulp-util';
-
-export default title => {
-    return function(err) {
-        gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
-        this.emit('end');
-    };
-};
